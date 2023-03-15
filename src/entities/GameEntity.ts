a3a4dfbd17c153070908f16ff2c75b20c06d9460
Angel Arcoraci <angel.arcoraci@gmail.com>
2023-03-15 17:33:17 +0100

    init

---
+++ src/entities/GameEntity.ts
@@ -0,0 +1,29 @@
+import { Box3, Mesh, Sphere, Vector3 } from "three";
+
+abstract class GameEntity {
+  protected _position: Vector3;
+  protected _mesh: Mesh = new Mesh();
+  public get mesh() {
+    return this._mesh;
+  }
+
+  protected _collider?: Box3 | Sphere;
+  public get collider() {
+    return this._collider;
+  }
+
+  constructor(position: Vector3) {
+    this._position = position;
+    this._mesh.position.set(
+      this._position.x,
+      this._position.y,
+      this._position.z
+    );
+  }
+
+  // methods
+  public load = async () => {};
+  public update = (_deltaT: number) => {};
+}
+
+export default GameEntity;
